0d3d4293db9913e70742e1c182cc65024fabb610
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-06-21 22:00:12 +0200

    Added new index.html file.

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -28,6 +28,9 @@ namespace WebApplication1
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
