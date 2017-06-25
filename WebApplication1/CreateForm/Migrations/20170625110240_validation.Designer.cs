1ca623ea7ed6a133ef46877d6478d36537141a0d
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-06-25 13:05:08 +0200

    Added validation migration.

--- WebApplication1/CreateForm/Migrations/20170625110240_validation.Designer.cs
+++ WebApplication1/CreateForm/Migrations/20170625110240_validation.Designer.cs
@@ -8,9 +8,10 @@ using CreateForm.Models;
 namespace CreateForm.Migrations
 {
     [DbContext(typeof(AlbumContext))]
-    partial class AlbumContextModelSnapshot : ModelSnapshot
+    [Migration("20170625110240_validation")]
+    partial class validation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
---
+++ WebApplication1/CreateForm/Migrations/20170625110240_validation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CreateForm.Migrations
+{
+    public partial class validation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Album",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Album",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+        }
+    }
+}
