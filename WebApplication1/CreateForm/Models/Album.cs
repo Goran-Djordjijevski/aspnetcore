c0dc2abd41239a9b0b7f7ad1d5e58779e8714fbe
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-06-25 12:02:09 +0200

    Added new Album class.

---
+++ WebApplication1/CreateForm/Models/Album.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateForm.Models
+{
+    public class Album
+    {
+        public int ID { get; set; }
+
+        public string Artist { get; set; }
+
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
