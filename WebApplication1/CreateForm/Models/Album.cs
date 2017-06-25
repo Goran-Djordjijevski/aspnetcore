c7e8947cff3be91a15134f99032bac0224d3a359
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-06-25 12:51:52 +0200

    Added validation.

--- WebApplication1/CreateForm/Models/Album.cs
+++ WebApplication1/CreateForm/Models/Album.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,12 +12,17 @@ namespace CreateForm.Models
 
         public string Artist { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 60, MinimumLength = 3)]
         public string Name { get; set; }
-
+        
         public string Genre { get; set; }
 
+        [Required]
         public DateTime ReleaseDate { get; set; }
 
+        [Required]
+        [Range(minimum: 0, maximum: 100.00)]
         public decimal Price { get; set; }
     }
 }
