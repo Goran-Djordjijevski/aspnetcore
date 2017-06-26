edc80f6315ace310de685ab91d032770e32336fd
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-06-26 21:26:33 +0200

    Added new webApi files.

--- WebApplication1/WebAPIs/Controllers/ProductsController.cs
+++ WebApplication1/WebAPIs/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebAPIs.Models;
 
 namespace WebAPIs.Controllers
 {
@@ -11,6 +12,30 @@ namespace WebAPIs.Controllers
     [Route("api/Products")]
     public class ProductsController : Controller
     {
-        public string Get() => "Hello API";
+        private static List<Product> products = new List<Product>()
+        {
+            new Product { ID = 1, Name = "Green Peppers" },
+            new Product { ID = 2, Name = "Soft Taco" },
+            new Product { ID = 3, Name = "Chipotle Sauce" }
+        };
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var product = products.SingleOrDefault(p => p.ID == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
     }
 }
